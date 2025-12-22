a48ed590ae405f46d516ad72ccd74e301920973e
ilke <117925325+iil-0@users.noreply.github.com>
2025-12-22 23:29:53 +0300

    serilog logging integrated

--- Program.cs
+++ Program.cs
@@ -2,38 +2,68 @@ using ECommerce.Api.Context;
 using ECommerce.Api.Middlewares;
 using ECommerce.Api.Services;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+// Serilog yapılandırması
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/ecommerce-.log", 
+        rollingInterval: RollingInterval.Day,
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
 
-// DbContext → PostgreSQL
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-// Services
-builder.Services.AddScoped<CategoryService>();
-builder.Services.AddScoped<ProductService>();
-builder.Services.AddScoped<UserService>();
-builder.Services.AddScoped<OrderService>();
-
-// Add services to the container
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Global Exception Handler - en üstte olmalı!
-app.UseGlobalExceptionHandler();
-
-// Configure the HTTP request pipeline
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Information("Starting ECommerce API...");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Serilog'u host'a ekle
+    builder.Host.UseSerilog();
+
+    // DbContext → PostgreSQL
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    // Services
+    builder.Services.AddScoped<CategoryService>();
+    builder.Services.AddScoped<ProductService>();
+    builder.Services.AddScoped<UserService>();
+    builder.Services.AddScoped<OrderService>();
+
+    // Add services to the container
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Global Exception Handler
+    app.UseGlobalExceptionHandler();
+
+    // Serilog request logging
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+
+    Log.Information("ECommerce API started successfully!");
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
